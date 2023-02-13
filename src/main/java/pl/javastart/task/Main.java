441160329906595d168cf3aad52c2a9b7fc688d9
soczystys <34406205+soczystys@users.noreply.github.com>
2023-02-13 20:01:52 +0100

    Poprawienie kodu według checkstyle'a

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,16 +3,14 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-        String product1Name, product2Name, product1Company, product2Company;
-        double product1Cost, product2Cost;
 
-        product1Name = "Mleko";
-        product1Company = "Mlekowita";
-        product1Cost = 2.5;
+        String product1Name = "Mleko";
+        String product1Company = "Mlekowita";
+        double product1Cost = 2.5;
 
-        product2Name = "Czekolada";
-        product2Company = "Wedel";
-        product2Cost = 2.19;
+        String product2Name = "Czekolada";
+        String product2Company = "Wedel";
+        double product2Cost = 2.19;
 
         System.out.println(product1Name + " " + product1Company + " " + product1Cost + "zł");
         System.out.println(product2Name + " " + product2Company + " " + product2Cost + "zł");
